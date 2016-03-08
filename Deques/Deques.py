ccc4fce0b6b6fc569deccd24daa869e5a8cd6229
bhushanwalke <bhushan.h2@gmail.com>
2016-03-08 00:28:16 -0800

    Queue Implementation

---
+++ Deques/Deques.py
@@ -0,0 +1,26 @@
+__author__ = 'bhushan'
+
+class Deques(object):
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def add_rear(self, item):
+        self.items.insert(0, item)
+
+    def add_front(self, item):
+        self.items.append(item)
+
+    def remove_front(self):
+        return self.items.pop()
+
+    def remove_rear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+
+
