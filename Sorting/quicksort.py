4eb0a1378bc0be9613a85d42f80b68c3950c3f39
bhushanwalke <bhushan.h2@gmail.com>
2016-03-19 14:24:22 -0700

    Quick Sort algorithm

--- Sorting/quicksort.py
+++ Sorting/quicksort.py
@@ -1,1 +1,10 @@
 __author__ = 'bhushan'
+
+def quick_sort(items_list):
+    quick_sort_helper(items_list, 0, len(items_list) - 1)
+
+def quick_sort_helper(items_list, first, last):
+    if first < last:
+        splitpoint = partition(items_list, first, last)
+
+        quick_sort_helper(items_list, first, splitpoint-1)
