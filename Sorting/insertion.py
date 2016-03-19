1e3cc43af50b63886453dbcc16524f0c23ea58aa
bhushanwalke <bhushan.h2@gmail.com>
2016-03-18 18:57:19 -0700

    Shell Sort Implemented

--- Sorting/insertion.py
+++ Sorting/insertion.py
@@ -1,1 +1,16 @@
 __author__ = 'bhushan'
+
+def insertion(items_list):
+    for index in range(1,len(items_list)):
+        currentValue = items_list[index]
+        position = index
+
+        while position > 0 and items_list[position-1] > items_list[position]:
+            items_list[position] = items_list[position-1]
+            items_list[position-1] = currentValue
+
+
+items_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+#items_list = [1,2,3,4,5,6,7]
+insertion(items_list)
+print items_list
--- Sorting/shellsort.py
+++ Sorting/shellsort.py
@@ -1,1 +1,28 @@
 __author__ = 'bhushan'
+
+
+def shellSort(items_list):
+    sublist = len(items_list)/2
+
+    while sublist > 0:
+        for start_pos in range(sublist):
+            gap_insertionSort(items_list, start_pos, sublist)
+
+        print(sublist, "", items_list)
+
+        sublist = sublist/2
+
+def gap_insertionSort(items_list, start, gap):
+    for i in range(start + gap, len(items_list), gap):
+        current_value = items_list[i]
+        position = i
+
+        while position >= gap and items_list[position-gap] > current_value:
+            items_list[position] = items_list[position-gap]
+            items_list[position-gap] = current_value
+            position = position-gap
+
+
+items_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+shellSort(items_list)
+print items_list
