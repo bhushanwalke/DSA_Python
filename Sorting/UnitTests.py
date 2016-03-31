a4e0261a2092d598fc31464fe3498185c96b3ece
bhushanwalke <bhushan.h2@gmail.com>
2016-03-30 21:13:02 -0700

    Unit Testing example

--- Sorting/UnitTests.py
+++ Sorting/UnitTests.py
@@ -1,1 +1,18 @@
 __author__ = 'bhushan'
+
+import unittest
+from Sorting import selection
+
+
+def cube(n):
+    "Return Cube"
+    return n ** 3
+
+class MyTest(unittest.TestCase):
+
+    def test_selection(self):
+        l1 = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+        l2 = [17, 20, 26, 31, 44, 54, 55, 77, 93]
+        selection(l1)
+        self.assertEqual(l1,l2)
+
