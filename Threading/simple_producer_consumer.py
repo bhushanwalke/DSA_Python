d43b38fb1f8c13985572e5cc959b2940f8b9a31a
bhushanwalke <bhushan.h2@gmail.com>
2016-04-01 16:11:08 -0700

    Simple Producer Consumer

--- Threading/simple_producer_consumer.py
+++ Threading/simple_producer_consumer.py
@@ -1,1 +1,47 @@
 __author__ = 'bhushan'
+
+import threading, random, time
+from Queue import Queue
+
+
+class Producer():
+    def __init__(self):
+        self.food = ['Ham', 'Soup', 'Salad']
+        self.nextTime = 0
+
+    def run(self):
+        global q
+        while(time.clock() < 10):
+            if(self.nextTime < time.clock()):
+                f = self.food[random.randrange(0, len(self.food))]
+                q.put(f)
+                print("Adding " + f)
+                self.nextTime += random.random()*2
+
+
+class Consumer():
+    def __init__(self):
+        self.nextTime = 0
+
+    def run(self):
+        global q
+        while(time.clock() < 10):
+            if self.nextTime < time.clock() and not q.empty():
+                f = q.get()
+                print("Removing " + f)
+                self.nextTime += random.random()*4
+
+
+
+if __name__ == '__main__':
+    q = Queue(10)
+
+    p = Producer()
+    c = Consumer()
+
+    pt = threading.Thread(target=p.run, args=())
+    ct = threading.Thread(target=c.run, args=())
+
+    pt.start()
+    ct.start()
+    print(threading.activeCount())
