22b4839738806a7595d8aff4df4d55447c9f68aa
Bhushan Walke <bhushan.h2@gmail.com>
2016-05-31 01:04:51 -0700

    word break

---
+++ leetcode/word_break.py
@@ -0,0 +1,26 @@
+def wordBreak(s, wordDict):
+    """
+    :type s: str
+    :type wordDict: Set[str]
+    :rtype: bool
+    """
+    n = len(s)
+    if not s or n == 0:
+        return False
+
+    dp = [False] * (n+1)
+    dp[0] = True
+
+    for i in range(1, n+1):
+        for j in range(i):
+            a = s[j:i]
+            if dp[j] and s[j:i] in wordDict:
+                dp[i] = True
+                break
+    return dp[-1]
+
+
+dict = ["leet", "code"]
+s = "codeleet"
+
+print wordBreak(s, dict)
