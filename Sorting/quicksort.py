99d0b74354f4e8ffd5ac3256b0010279820341c2
Bhushan Walke <bhushan.h2@gmail.com>
2016-06-06 15:29:08 -0700

    quicksort, revised again

--- Sorting/quicksort.py
+++ Sorting/quicksort.py
@@ -8,3 +8,22 @@ def quick_sort_helper(items_list, first, last):
         splitpoint = partition(items_list, first, last)
 
         quick_sort_helper(items_list, first, splitpoint-1)
+        quick_sort_helper(items_list, splitpoint+1, last)
+
+def partition(items_list, first, last):
+    print "list now ", items_list[first:last]
+    pivot = items_list[last]
+    pindex = first
+
+    for i in range(last-1):
+        if items_list[i] <= pivot:
+            items_list[i], items_list[pindex] = items_list[pindex], items_list[i]
+            pindex +=1
+
+    items_list[pindex], items_list[last] = items_list[last], items_list[pindex]
+    return pindex
+
+items_list = [5,4,3,2,1]
+quick_sort(items_list)
+print items_list
+
